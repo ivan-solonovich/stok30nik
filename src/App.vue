e48ba2e467680892dd865275c5b1a359dfab22b5
ivan-solonovich <solonovichivan1978@gmail.com>
2023-11-17 08:09:30 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import MainWrapper from "./components/MainWrapper.vue";
+</script>
+
+<template>
+  <MainWrapper/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainWrapper.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import NavbarMain from "./navbar/NavbarMain.vue";
+import HeaderSection from "./headerSeaction/HeaderSection.vue";
+</script>
+
+<template>
+  <NavbarMain/>
+  <HeaderSection/>
+</template>
+
+<style scoped>
+
+</style>

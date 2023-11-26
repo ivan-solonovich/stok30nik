c05681465fd88d6aab1abdaa6d9886203d61dcb7
ivan-solonovich <solonovichivan1978@gmail.com>
2023-11-26 17:49:07 +0300

    First commit - catalog pic

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import AxiosPlugin from 'vue-axios-cors';
 import router from "./router/router.js";
 import store from "./store/store.js";
 import './style.css'
@@ -8,7 +9,8 @@ import App from './App.vue'
 const app = createApp(App);
     app.use(
         router,
-        store
+        store,
+        AxiosPlugin
         )
 
         .mount('#app')

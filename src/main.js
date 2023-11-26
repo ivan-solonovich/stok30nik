a7f4a90bbfe83da724299c37d6b95d14ac5bbdd5
ivan-solonovich <solonovichivan1978@gmail.com>
2023-11-26 18:00:26 +0300

    First commit - catalog pic

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
-
 import router from "./router/router.js";
 import store from "./store/store.js";
 import './style.css'
 import App from './App.vue'
-
 
 const app = createApp(App);
     app.use(
